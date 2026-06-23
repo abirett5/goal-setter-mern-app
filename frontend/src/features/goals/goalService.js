7ffd313c404b6ef6892e3b37db4a2cc25695a700
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2026-06-23 12:41:22 +0600

    Updated

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/goals/'
+const API_URL = 'https://goal-setter-mern-app-60cz.onrender.com/api/goals/'
 
 // Create new goal
 const createGoal = async (goalData, token) => {
